f5ca320109f6d9c8d9840347db0a04e9a6de9900
Blair Saunders <bsaunders@smu.edu>
2025-04-28 15:13:59 -0500

    minor changes to hero photo

--- app/components/FloorplanTeaser.tsx
+++ app/components/FloorplanTeaser.tsx
@@ -14,7 +14,7 @@ export default function FloorplanTeaser({ floorplans }: Props) {
   return (
     <section className="py-8 px-10 bg-stone-300">
       <div className="container mx-auto px-4">
-        <h2 className={`text-3xl mb-8 pt-2 pb-2 text-center text-stone-600 font-semibold border-2 bg-stone-50 ${montserrat.className}`}>FEATURED FLOORPLANS</h2>
+        <h2 className={`text-3xl mb-8 pt-2 pb-2 text-center text-stone-600 font-semibold ${montserrat.className}`}>FEATURED FLOORPLANS</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {floorplans.map((plan) => (
             <FloorplanCard key={plan.id} plan={plan} />
--- app/components/HeroPhoto.tsx
+++ app/components/HeroPhoto.tsx
@@ -6,15 +6,15 @@ const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function HeroPhoto() {
     return (
-      <div className="w-full h-[60vh] bg-gray-100 relative overflow-hidden">
+      <div className="w-full h-[60vh] sm:h-[90vh] bg-gray-100 relative overflow-hidden">
         <img
           src="/images/amenities/unit-kitchen.jpeg" // Will create this in step 3
           alt="The Evelyn Apartments"
           className="w-full h-full object-cover"
           loading="eager"
         />
-        <div className="absolute top-40 left-0 right-0 text-center">
-          <h1   className={`text-4xl sm:text-5xl md:text-6xl font-bold text-stone-50 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] ${montserrat.className}`}>
+        <div className="absolute top-60 left-0 right-0 text-center">
+          <h1   className={`text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-stone-50 drop-shadow-[0_2.3px_2.3px_rgba(0.8,0.8,0.8,0.8)] ${montserrat.className}`}>
             THE EVELYN RESIDENCES
           </h1>
         </div>
