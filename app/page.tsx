8470d7bbfa42d4b153080cb5d2cdc6a7331a336e
Blair Saunders <bsaunders@smu.edu>
2025-05-05 16:59:44 -0500

    added neighborhood action to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@
 
 import Hero from './components/Hero'
 import FloorplanHomeContent from './components/FloorplanHomeContent'
+import NeighborhoodSection from './amenities/NeighborhoodSection';
+
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FloorplanHomeContent /> {/* Different component from FloorplanGrid */}
+      <NeighborhoodSection />
     </>
   )
 }
